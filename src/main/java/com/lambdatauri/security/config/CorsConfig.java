79daad3a8cb2225037c0894f37c93e88eaa719f8
Oliviu Gratii <oliviu.gratii@edu.unito.it>
2023-12-09 02:56:04 +0100

    [gcp-cloud-build-deploy-cloud-run]

--- src/main/java/com/lambdatauri/security/config/CorsConfig.java
+++ src/main/java/com/lambdatauri/security/config/CorsConfig.java
@@ -8,9 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "PUT", "DELETE")
-                .allowedHeaders("*");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
     }
 }
